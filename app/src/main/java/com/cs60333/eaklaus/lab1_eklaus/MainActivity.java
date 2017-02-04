6e5637456dbbaa2748d0193e38076deae7031c9f
Erik Klaus <Erik Klaus>
2017-02-03 21:15:16 -0500

    Initial commit

---
+++ app/src/main/java/com/cs60333/eaklaus/lab1_eklaus/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cs60333.eaklaus.lab1_eklaus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
